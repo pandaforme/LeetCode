a7e3404f9cdf3393644f9c206d2fc95c88eb967b
ChienkuChen <chienku_chen@htc.com>
2018-01-28 21:32:34 +0800

    Fix bugs

--- src/_692/692.top-k-frequent-words.java
+++ src/_692/692.top-k-frequent-words.java
@@ -21,61 +21,61 @@ class Solution {
     // using priority queue
     // time complexity: O(n) + O(n * log k)
     // space complexity: O(n) + O(k)
-//    public List<String> topKFrequent(String[] words, int k) {
-//        Map<String, Integer> map = new HashMap<>();
-//        for (String word : words) {
-//            map.put(word, map.getOrDefault(word, 0) + 1);
-//        }
-//
-//        Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((o1, o2) -> {
-//            if (o1.getValue().equals(o2.getValue()))
-//                return o1.getKey().compareTo(o2.getKey());
-//            else
-//                return o2.getValue().compareTo(o1.getValue());
-//        });
-//
-//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-//            queue.offer(entry);
-//            if (queue.size() > k)
-//                queue.poll();
-//        }
-//
-//        return queue.stream().map(Map.Entry::getKey).collect(Collectors.toList());
-//    }
-
-
-    // using bucket sort
-    // time complexity: O(n)
-    // space complexity: O(n) + O(k)
     public List<String> topKFrequent(String[] words, int k) {
         Map<String, Integer> map = new HashMap<>();
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        List<String>[] bucket = new List[words.length + 1];
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (bucket[entry.getValue()] == null)
-                bucket[entry.getValue()] = new ArrayList<>();
+        Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((o1, o2) -> {
+            if (o1.getValue().equals(o2.getValue()))
+                return o2.getKey().compareTo(o1.getKey());
+            else
+                return o1.getValue().compareTo(o2.getValue());
+        });
 
-            bucket[entry.getValue()].add(entry.getKey());
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            queue.offer(entry);
+            if (queue.size() > k)
+                queue.poll();
         }
 
         List<String> result = new ArrayList<>();
-        for (int i = bucket.length - 1; i > 0; i--) {
-            if (bucket[i] != null) {
-                bucket[i].sort(Comparator.naturalOrder());
-
-                for (String s : bucket[i]) {
-                    if (result.size() < k)
-                        result.add(s);
-                    else
-                        return result;
-                }
-            }
+        while (!queue.isEmpty()){
+            result.add(0, queue.poll().getKey());
         }
+
         return result;
     }
+
+
+    // using bucket sort
+    // time complexity: O(n)
+    // space complexity: O(n) + O(k)
+//    public List<String> topKFrequent(String[] words, int k) {
+//        Map<String, Integer> map = new HashMap<>();
+//        for (String word : words) {
+//            map.put(word, map.getOrDefault(word, 0) + 1);
+//        }
+//
+//        List<String>[] bucket = new List[words.length + 1];
+//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            if (bucket[entry.getValue()] == null)
+//                bucket[entry.getValue()] = new ArrayList<>();
+//
+//            bucket[entry.getValue()].add(entry.getKey());
+//        }
+//
+//        List<String> result = new ArrayList<>();
+//        for (int i = bucket.length - 1; i > 0; i--) {
+//            if (bucket[i] != null) {
+//                bucket[i].sort(Comparator.naturalOrder());
+//                result.addAll(bucket[i]);
+//            }
+//        }
+//
+//        return result.size() > k ? result.subList(0, k) : result;
+//    }
 //    public List<String> topKFrequent(String[] words, int k) {
 //        Map<String, Integer> map = new HashMap<>();
 //        for (String word : words) {
