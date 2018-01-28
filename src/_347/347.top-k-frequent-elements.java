e750be5e3d25890b701e796e13830655f72af8d4
ChienkuChen <chienku_chen@htc.com>
2018-01-28 21:35:11 +0800

    Add 347

---
+++ src/_347/347.top-k-frequent-elements.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (bucket[entry.getValue()] == null)
+                bucket[entry.getValue()] = new ArrayList<>();
+            bucket[entry.getValue()].add(entry.getKey());
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = bucket.length - 1; i > 0 && result.size() < k; i--) {
+            if (bucket[i] != null)
+                result.addAll(bucket[i]);
+        }
+
+        return result.size() > k ? result.subList(0, k) : result;
+    }
+}
