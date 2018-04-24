5a8720c9655814144f6f9a336d9b7e2f90636f93
ChienkuChen <chienku_chen@htc.com>
2018-04-24 17:34:29 +0800

    Add 22

---
+++ src/_22/22.generate-parentheses.java
@@ -0,0 +1,32 @@
+package _22;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+
+        helper(n, n, "", result);
+
+        return result;
+    }
+
+    private void helper(int left, int right, String tmp, List<String> result) {
+        if (left > right)
+            return;
+
+        if (left > 0)
+            helper(left - 1, right, tmp + "(", result);
+
+        if (right > 0)
+            helper(left, right - 1, tmp + ")", result);
+
+        if (left == 0 && right == 0)
+            result.add(tmp);
+    }
+    public static void main(String[] args) {
+        System.out.println(new Solution().generateParenthesis(2));
+    }
+
+}
