8aa000c8c0d30522b7d51d43a02782470bee84f2
ChienkuChen <chienku_chen@htc.com>
2018-04-28 00:26:13 +0800

    Redo 215

--- src/_215/215.kth-largest-element-in-an-array.java
+++ src/_215/215.kth-largest-element-in-an-array.java
@@ -1,52 +1,58 @@
-package _215;
-
 class Solution {
-//    public int findKthLargest(int[] nums, int k) {
-//        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));
-//
-//        for (int num : nums) {
-//            queue.offer(num);
-//        }
-//
-//        int result = 0;
-//        while (k != 0) {
-//            if (queue.isEmpty())
-//                return -1;
-//
-//            result = queue.poll();
-//            k--;
-//        }
-//
-//        return result;
-//    }
-
     public int findKthLargest(int[] nums, int k) {
-        return find(nums, nums.length - k, 0, nums.length - 1);
+        return findKthSmallest(nums, 0, nums.length - 1, nums.length - k + 1);
     }
 
-    private int find(int[] nums, int k, int start, int end) {
-        if (start > end) return Integer.MAX_VALUE;
-
+    private int findKthSmallest(int[] nums, int start, int end, int k) {
+        int i = start;
+        int j = end - 1;
         int pivot = nums[end];
 
-        int left = start;
-        for (int i = start; i < end; i++) {
-            if (nums[i] <= pivot) {
-                swap(nums, left, i);
-                left++;
+        while (i <= j) {
+            while (i < end && nums[i] <= pivot) {
+                i++;
             }
+
+            while (j >= start && nums[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j)
+                swap(nums, i, j);
         }
 
-        swap(nums, left, end);
+        swap(nums, i, end);
 
-        if (left == k) {
-            return nums[left];
-        } else if (left > k) {
-            return find(nums, k, start, left - 1);
-        } else {
-            return find(nums, k, left + 1, end);
-        }
+        int pth = i - start + 1;
+        if (pth == k)
+            return nums[i];
+        else if (k < pth)
+            return findKthSmallest(nums, start, i - 1, k);
+        else
+            return findKthSmallest(nums, i + 1, end, k - pth);
     }
+
+//    private int findKthSmallest(int[] nums, int start, int end, int k) {
+//        int pivot = nums[end];
+//        int left = start;
+//
+//        for (int i = start; i < end; i++) {
+//            if (nums[i] <= pivot) {
+//                swap(nums, left, i);
+//                left++;
+//            }
+//        }
+//        swap(nums, left, end);
+//
+//        int pth = left - start + 1;
+//        if (pth == k)
+//            return nums[left];
+//        else if (k < pth)
+//            return findKthSmallest(nums, start, left - 1, k);
+//        else
+//            return findKthSmallest(nums, left + 1, end, k - pth);
+//    }
+
 
     private void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
@@ -55,7 +61,8 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
-        System.out.println(new Solution().findKthLargest(new int[]{3, 2, 1, 5, 6, 4, 5, 6, 6, 6, 6}, 6));
+        System.out.println(new Solution().findKthLargest(new int[]{3, 2, 1, 5, 6, 4, 1}, 6));
+        System.out.println(new Solution().findKthLargest(new int[]{1, 2, 3, 4, 5, 6, 7}, 3));
+        System.out.println(new Solution().findKthLargest(new int[]{7, 6, 5, 4, 3, 2, 1}, 3));
     }
 }
