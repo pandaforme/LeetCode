e6208fb7573ced0358294e8fe3d5421493b3cc55
ChienkuChen <chienku_chen@htc.com>
2018-04-29 16:58:04 +0800

    Add interval data structure

--- src/datastructure/Interval.java
+++ src/datastructure/Interval.java
@@ -1,4 +1,24 @@
 package datastructure;
 
 public class Interval {
+    public int start;
+    public int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Interval [" +
+                "start=" + start +
+                ", end=" + end +
+                "]";
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
 }
