bdfb18552db09c9b50e718e79f26ff6f0d7b6617
ChienkuChen <chienku_chen@htc.com>
2018-04-30 15:25:40 +0800

    Add 252

--- src/_252/Solution.java
+++ src/_252/Solution.java
@@ -1,4 +1,31 @@
 package _252;
 
+import datastructure.Interval;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        if (intervals.length == 0 || intervals.length == 1)
+            return true;
+
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o.start));
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (isOverlap(intervals[i - 1], intervals[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+
+    private boolean isOverlap(Interval interval1, Interval interval2) {
+        return interval1.start < interval2.end && interval2.start < interval1.end;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().canAttendMeetings(new Interval[]{new Interval(13, 20), new Interval(13, 17)}));
+    }
 }
