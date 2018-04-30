0118e289997cfd78aad90c6c701ed5b866dcf23d
ChienkuChen <chienku_chen@htc.com>
2018-04-30 15:49:00 +0800

    Add 74

--- src/_74/74.search-a-2d-matrix.java
+++ src/_74/74.search-a-2d-matrix.java
@@ -4,36 +4,58 @@ import java.util.Arrays;
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix.length == 0 || matrix[0].length == 0)
+        if (matrix.length == 0)
+            return false;
+        if (matrix[0].length == 0)
             return false;
 
-        boolean exist = isExist(0, matrix.length - 1, target, matrix);
-        System.out.println(exist);
-        return exist;
+        return helper(matrix, target, 0, matrix.length - 1);
     }
 
-    private boolean isExist(int startRow, int endRow, int target, int[][] matrix) {
-        if (startRow > endRow)
+    private boolean helper(int[][] matrix, int target, int start, int end) {
+        if (start > end)
             return false;
 
-        int middleRow = startRow + (endRow - startRow) / 2;
-        int lastPivot = matrix[middleRow][matrix[0].length - 1];
-        int startPivot = matrix[middleRow][0];
-
-        if (target == lastPivot || target == startPivot)
+        int middle = (start + end) / 2;
+        int search = Arrays.binarySearch(matrix[middle], target);
+        if (search >= 0)
             return true;
-
-        if (startPivot < target && target < lastPivot)
-            return Arrays.binarySearch(matrix[middleRow], target) >= 0;
-
-        if (target < lastPivot)
-            return isExist(startRow, middleRow - 1, target, matrix);
-        else
-            return isExist(middleRow + 1, endRow, target, matrix);
+        else {
+            if (target > matrix[middle][matrix[0].length - 1])
+                return helper(matrix, target, middle + 1, end);
+            else
+                return helper(matrix, target, start, middle - 1);
+        }
     }
+
 
     public static void main(String[] args) {
-        new Solution().searchMatrix(new int[][]{
-                {1, 3}}, 1);
+        System.out.println(new Solution().searchMatrix(
+                new int[][]{
+                        new int[]{},
+                }, 1
+        ));
+
+        System.out.println(new Solution().searchMatrix(
+                new int[][]{
+                        new int[]{1},
+                }, 1
+        ));
+
+        System.out.println(new Solution().searchMatrix(
+                new int[][]{
+                        new int[]{1, 3, 5, 7},
+                        new int[]{10, 11, 16, 20},
+                        new int[]{23, 30, 34, 50}
+                }, 13
+        ));
+
+        System.out.println(new Solution().searchMatrix(
+                new int[][]{
+                        new int[]{1, 3, 5, 7},
+                        new int[]{10, 11, 16, 20},
+                        new int[]{23, 30, 34, 50}
+                }, 3
+        ));
     }
 }
