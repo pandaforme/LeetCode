5aa14dfeb9e4f5cc646a440c2aa7e76679739362
ChienkuChen <chienku_chen@htc.com>
2018-04-30 16:01:05 +0800

    Redo 240

--- src/_240/240.search-a-2d-matrix-ii.java
+++ src/_240/240.search-a-2d-matrix-ii.java
@@ -6,46 +6,24 @@ class Solution {
         if (matrix.length == 0)
             return false;
 
-        int i = 0;
-        int j = matrix[0].length - 1;
+        if (matrix[0].length == 0)
+            return false;
 
-        while (i < matrix.length && j >= 0) {
-            if (matrix[i][j] == target)
-                return true;
-
-            if (target > matrix[i][j])
-                i++;
-            else
-                j--;
-        }
-
-        return false;
+        return helper(matrix, target, 0, matrix[0].length - 1);
     }
 
+    private boolean helper(int[][] matrix, int target, int i, int j) {
+        if (i >= matrix.length || j < 0)
+            return false;
 
-//    public boolean searchMatrix(int[][] matrix, int target) {
-//        if (matrix.length == 0)
-//            return false;
-//
-//        boolean exist = isExist(0, matrix[0].length - 1, target, matrix);
-//        System.out.println(exist);
-//        return exist;
-//    }
-//
-//    private boolean isExist(int startRow, int startColumn, int target, int[][] matrix) {
-//        if (startRow >= matrix.length || startColumn < 0)
-//            return false;
-//
-//        int pivot = matrix[startRow][startColumn];
-//
-//        if (target == pivot)
-//            return true;
-//
-//        if (target < pivot)
-//            return isExist(startRow, startColumn - 1, target, matrix);
-//        else
-//            return isExist(startRow + 1, startColumn, target, matrix);
-//    }
+        if (target == matrix[i][j])
+            return true;
+
+        if (target < matrix[i][j])
+            return helper(matrix, target, i, j - 1);
+        else
+            return helper(matrix, target, i + 1, j);
+    }
 
     public static void main(String[] args) {
         System.out.println(new Solution().searchMatrix(new int[][]{
@@ -53,6 +31,6 @@ class Solution {
                 {2, 5, 8, 12, 19},
                 {3, 6, 9, 16, 22},
                 {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}}, 18));
+                {18, 21, 23, 26, 30}}, 99));
     }
 }
