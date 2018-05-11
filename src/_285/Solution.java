cdde9859a7d8a7a7b19411aa70eeb15972cd0a16
ChienkuChen <chienku_chen@htc.com>
2018-05-11 18:13:57 +0800

    Redo 285

--- src/_285/Solution.java
+++ src/_285/Solution.java
@@ -6,36 +6,21 @@ public class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         if (root == null || p == null)
             return null;
-
-        TreeNode result = null;
-        while (root != null) {
-            if (p.val < root.val) {
-                result = root;
-                root = root.left;
-            } else {
-                root = root.right;
-            }
+        if (p.val < root.val) {
+            TreeNode successor = inorderSuccessor(root.left, p);
+            return successor == null ? root : successor;
+        } else {
+            return inorderSuccessor(root.right, p);
         }
-
-        return result;
     }
-
-    // recursive solution
-//    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-//        if (root == null || p == null)
-//            return null;
-//
-//        if (p.val < root.val) {
-//            TreeNode successor = inorderSuccessor(root.left, p);
-//            return successor == null ? root : successor;
-//        }else {
-//            return inorderSuccessor(root.right, p);
-//        }
-//    }
 
     public static void main(String[] args) {
         TreeNode one = new TreeNode(1);
         TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode four = new TreeNode(4);
+        TreeNode five = new TreeNode(5);
+
         two.left = one;
 
         System.out.println(new Solution().inorderSuccessor(two, one).val);
