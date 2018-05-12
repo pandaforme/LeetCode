c5d910df1fd7c7c90e7565fe80b70c51836ceca1
ChienkuChen <chienku_chen@htc.com>
2018-05-12 23:30:37 +0800

    Redo 39

--- src/_39/39.combination-sum.java
+++ src/_39/39.combination-sum.java
@@ -1,39 +1,41 @@
 package _39;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        return find(candidates, target, 0);
-    }
-
-    public List<List<Integer>> find(int[] candidates, int target, int start) {
-        if (target <= 0)
+        if (candidates == null || candidates.length == 0)
             return new ArrayList<>();
 
-        List<List<Integer>> results = new ArrayList<>();
-        for (int i = start; i < candidates.length; i++) {
-            if (target - candidates[i] == 0) {
-                List<Integer> list = new ArrayList<>();
-                list.add(candidates[i]);
-                results.add(list);
-                return results;
-            }
+        List<List<Integer>> result = new ArrayList<>();
 
-            List<List<Integer>> lists = find(candidates, target - candidates[i], i);
-            for (List<Integer> list : lists) {
-                list.add(candidates[i]);
-                results.add(list);
-            }
+        helper(candidates, target, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void helper(int[] candidates, int target, int start, List<Integer> tmp, List<List<Integer>> result) {
+        if (target < 0)
+            return;
+
+        if (target == 0) {
+            result.add(tmp);
+            return;
         }
 
-        return results;
+        if (start >= candidates.length)
+            return;
+
+        helper(candidates, target, start + 1, tmp, result);
+
+        List<Integer> list = new ArrayList<>(tmp);
+        list.add(candidates[start]);
+        helper(candidates, target - candidates[start], start, list, result);
     }
 
     public static void main(String[] args) {
+        System.out.println(new Solution().combinationSum(new int[]{2, 3, 5}, 8));
         System.out.println(new Solution().combinationSum(new int[]{2, 3, 6, 7}, 7));
     }
 }
